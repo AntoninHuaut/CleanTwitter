544186d6d0e489dc46c4f5d9bdc8c96b7845f2a6
Antonin <antonin.huaut+github@gmail.com>
2020-10-13 18:48:25 +0200

    1.1.0

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const filter = {
 const webRequestFlags = ['blocking'];
 
 browser.webRequest.onBeforeRequest.addListener(page => {
-        if (isActiveRun()) return;
+        if (isActiveRun() || disableExtension) return;
 
         return {
             cancel: true,
@@ -16,35 +16,62 @@ browser.webRequest.onBeforeRequest.addListener(page => {
 
 const exclude = {
     timeout: null,
-    timeout_duration: 1000 * 5,
+    timeout_duration: 1000 * 15,
     runnable: null,
     timer: 0
-}
+};
+
+var disableExtension = false;
 
 browser.browserAction.onClicked.addListener(() => {
-    if (!isActiveRun()) {
+    if (disableExtension) {
+        stopRunnable();
+        disableExtension = false;
+    } else if (!isActiveRun()) {
         chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
         chrome.browserAction.setBadgeTextColor({ color: [255, 255, 255, 255] });
 
         exclude.runnable = setInterval(updateBadge, 1000);
         updateBadge();
 
-        exclude.timeout = setTimeout(() => {
-            clearInterval(exclude.runnable);
-            exclude.timeout = null;
-            exclude.runnable = null;
-            exclude.timer = 0;
-            chrome.browserAction.setBadgeText({ text: null });
-        }, exclude.timeout_duration);
+        exclude.timeout = setTimeout(stopRunnable, exclude.timeout_duration);
+    } else {
+        stopRunnable();
+        disableExtension = true;
+        setBadgeText("∞");
     }
+
+    setIcon();
 });
+
+function setIcon() {
+    const path = isActiveRun() || disableExtension ? "icons/icon_color.png" : "icons/icon_black.png";
+    chrome.browserAction.setIcon({
+        path: path
+    });
+}
+
+function stopRunnable() {
+    if (!!exclude.runnable) clearInterval(exclude.runnable);
+    if (!!exclude.timeout) clearTimeout(exclude.timeout);
+
+    exclude.timeout = null;
+    exclude.runnable = null;
+    exclude.timer = 0;
+    setBadgeText(null);
+    setIcon();
+}
 
 function updateBadge() {
     const time = exclude.timeout_duration / 1000 - exclude.timer;
-    chrome.browserAction.setBadgeText({ text: `${time}` });
+    setBadgeText(`${time}`);
     exclude.timer++;
 }
 
 function isActiveRun() {
     return !!exclude.timeout;
+}
+
+function setBadgeText(text) {
+    chrome.browserAction.setBadgeText({ text: text });
 }
